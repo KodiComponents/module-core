a54c48eefb730712d3bd8b5ae6123fecdb8c62ce
butschster <butschster@gmail.com>
2016-02-15 17:12:52 +0300

    Убрана публикация конфига, т.к. он создает проблемы в работе системы

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -53,10 +53,6 @@ class ModuleServiceProvider extends ServiceProvider
         });
 
         $this->publishes([
-            __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'cms.php' => config_path('cms.php')
-        ], 'kodicms');
-
-        $this->publishes([
              __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'cms' => public_path('cms')
          ], 'kodicms');
 
