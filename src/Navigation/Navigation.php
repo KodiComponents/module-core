4b1bd7f18db931a3e4fb6e3ce155de078060a29b
Butschster <butschster@gmail.com>
2016-05-05 09:55:20 +0300

    Add new navigation class

--- src/Navigation/Navigation.php
+++ src/Navigation/Navigation.php
@@ -2,6 +2,8 @@
 
 namespace KodiCMS\CMS\Navigation;
 
+use KodiComponents\Navigation\Contracts\PageInterface;
+
 class Navigation extends \KodiComponents\Navigation\Navigation
 {
 
@@ -17,5 +19,29 @@ class Navigation extends \KodiComponents\Navigation\Navigation
         return view('cms::navigation.navigation', [
             'pages' => $this->toArray(),
         ])->render();
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return PageInterface
+     */
+    protected function createPageFromArray(array $data)
+    {
+        $page = app(PageInterface::class);
+
+        foreach ($data as $key => $value) {
+            if ($key != 'children' and method_exists($page, $method = 'set'.ucfirst($key))) {
+                $page->{$method}($value);
+            }
+        }
+
+        if (isset($data['children']) and is_array($data['children'])) {
+            foreach ($data['children'] as $child) {
+                $page->addPage($child);
+            }
+        }
+
+        return $page;
     }
 }
