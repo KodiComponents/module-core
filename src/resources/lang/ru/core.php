3771c253f0c5b6d2e77306914b8a16916eb9455a
Butschster <butschster@gmail.com>
2016-05-26 22:54:06 +0300

    Добавлен перевод для исключения ModelNotFoundException

--- src/resources/lang/ru/core.php
+++ src/resources/lang/ru/core.php
@@ -1,40 +1,40 @@
 <?php
 
 return [
-    'title'      => [
-        'about'     => 'О системе',
-        'settings'  => 'Настройки',
-        'system'    => 'Системные',
+    'title' => [
+        'about' => 'О системе',
+        'settings' => 'Настройки',
+        'system' => 'Системные',
         'dashboard' => 'Рабочий стол',
-        'design'    => 'Дизайн',
-        'system'    => 'Система',
-        'update'    => 'Обновление',
+        'design' => 'Дизайн',
+        'system' => 'Система',
+        'update' => 'Обновление',
     ],
-    'label'      => [
-        'not_set'       => 'Не указан',
-        'date_never'    => 'Никогда',
+    'label' => [
+        'not_set' => 'Не указан',
+        'date_never' => 'Никогда',
         'toggler_close' => 'Свернуть',
-        'toggler_open'  => 'Развернуть',
-        'update'        => [
-            'new_files'     => 'Новые файлы',
+        'toggler_open' => 'Развернуть',
+        'update' => [
+            'new_files' => 'Новые файлы',
             'changed_files' => 'Измененные файлы',
         ],
     ],
-    'button'     => [
-        'update'       => 'Сохранить',
+    'button' => [
+        'update' => 'Сохранить',
         'update_close' => 'Сохранить и закрыть',
-        'create'       => 'Создать',
+        'create' => 'Создать',
         'create_close' => 'Создать и закрыть',
-        'cancel'       => 'Отмена',
-        'bug_report'   => 'Сообщить об ошибке',
+        'cancel' => 'Отмена',
+        'bug_report' => 'Сообщить об ошибке',
     ],
-    'helpers'    => [
-        'not_select'  => 'Не выбран',
-        'key'         => 'Ключ',
+    'helpers' => [
+        'not_select' => 'Не выбран',
+        'key' => 'Ключ',
         'description' => 'Описание',
     ],
-    'footer'     => [
-        'poweredBy'   => 'Работает на :framework v:version',
+    'footer' => [
+        'poweredBy' => 'Работает на :framework v:version',
         'adminTeheme' => 'Тема :name',
     ],
     'navigation' => [
@@ -42,12 +42,13 @@ return [
         'site' => 'Посмотреть сайт',
         '',
     ],
-    'messages'   => [
-        'route_not_found'    => 'Страница не найдена',
-        'cache_clear'        => 'Кеш очищен',
-        'are_you_sure'       => 'Вы уверены?',
-        'new_version'        => 'Имеется более новая версия системы (:version)',
-        'no_new_version'     => 'У вас установлена последняя версия.',
+    'messages' => [
+        'model_not_found' => 'Объект не найден',
+        'route_not_found' => 'Страница не найдена',
+        'cache_clear' => 'Кеш очищен',
+        'are_you_sure' => 'Вы уверены?',
+        'new_version' => 'Имеется более новая версия системы (:version)',
+        'no_new_version' => 'У вас установлена последняя версия.',
         'update_information' => 'Если система клонирована из репозитория Github, используйте команду <strong>git pull</strong> для обновления',
     ],
 ];
